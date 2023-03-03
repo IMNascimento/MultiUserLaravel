7d34f5ee86130ddd4ac16bb6857eeeb10a33c2b9
Igor Nascimento <trabalho.computador.sjn@gmail.com>
2023-03-03 16:21:20 -0300

    foi adicionado o recebimento de paramentros do request para cadastro do usuario

--- MultiUser/app/Http/Controllers/Auth/RegisteredUserController.php
+++ MultiUser/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,12 +34,14 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'type'=>['required','string'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'type'=>$request->type,
         ]);
 
         event(new Registered($user));
