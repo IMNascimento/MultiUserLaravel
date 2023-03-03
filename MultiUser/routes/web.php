c0a3db2da0a5269362d17e85c9d37f0ae7c6d68c
Igor Nascimento <trabalho.computador.sjn@gmail.com>
2023-03-03 17:49:59 -0300

    foi terminado de fazer as duas rotas protegidas

--- MultiUser/routes/web.php
+++ MultiUser/routes/web.php
@@ -18,9 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cliente/dashboard', function () {
+    return view('user');
+})->middleware(['user', 'verified']);
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('admin');
+})->middleware(['admin', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
